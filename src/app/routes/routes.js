d4e941732a1a8a2b08fc7a6f19356b4bb1d66277
Camilo Ibarra <camilo.ibarra@kapta.biz>
2018-02-09 16:22:06 -0500

    funcion lista, para filtrar registrados con id de instructor

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -139,22 +139,31 @@ module.exports = (app, passport) => {
     //Vista simple para SignUp (Todos los usuarios)
 
 
-    /*----------------- REGISTRO POST---------------*/
+    /*----------------- REGISTRO POST-------------------*/
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect: '/profile',
         failureRedirect: '/signup',
         failureFlash: true
     }));
 
-    /*--------------- VIEW PERFIL -------------*/
+    /*------------------- VIEW PERFIL ------------------*/
     app.get('/profile', isLoggedIn, (req, res) => {
         res.render('profile', {
-            user: req.user
+            user: req.user,
+            //sesion: req.session.nuevaSesion
         });
     });
 
-    
-    /*------------------ LOGOUT ------------------*/
+    /*------------------- VIEW PERFIL ------------------*/
+    app.get('/statistics', isLoggedIn, (req, res) => {
+        res.render('statistics', {
+            user: req.user,
+            //sesion: req.session.nuevaSesion
+        });
+    });
+
+
+    /*-------------------- LOGOUT ----------------------*/
     app.get('/logout', (req, res) => {
         req.logout();
         res.redirect('/');
@@ -171,7 +180,40 @@ module.exports = (app, passport) => {
     }
 
 
-    /* ----------- LOGIN SUPER ADMIN ------------*/
+    /* ------- FUNCION PARA ENCONTRAR INSCRITOS --------*/
+
+    function lista(req, res, next) {
+
+        var id = req.user.id;
+        var newTestSchema = new TestSchema();
+
+        TestSchema.find({id_inst: id}, function(err, test){
+            if(err){
+                console.log(err);
+                    console.log(err); 
+                }
+                if(!test){
+                    console.log("no hay inscritos")
+                    return next();
+                }else{
+                    res.json(test);
+                    return next();
+                }
+        });
+    }
+
+
+    /* --------------- REGISTRAR INSTRUCTOR -------------*/
+    //Usa funcion isLoggedIn para acceder a id, luego lista y busca id_inst = id
+    
+    app.get('/list', isLoggedIn, lista, (req, res) => {
+        res.render('list', {
+            user: req.user.id_inst
+        });
+    }) 
+
+
+    /* ---------------- LOGIN SUPER ADMIN --------------*/
     //Autentica a SuperAdmin comprobando que req.user.sa exista
     
     app.get('/signup', isLoggedIn, (req, res) => {
@@ -186,17 +228,19 @@ module.exports = (app, passport) => {
         console.log("No autorizado");
     });
 
-    /* ----------- REGISTRAR INSTRUCTOR -------------*/
-
+    /* --------------- REGISTRAR INSTRUCTOR -------------*/
+    
     app.get('/new', isLoggedIn, (req, res) => {
         res.render('new', {
+            user: req.user,
             message: req.flash('signupMessage')
         });
     }) 
 
 
-    /*----- CONSULTA JSON COLLECCION testSchemas ----*/
+    /*----- CONSULTA JSON COLLECCION testSchemas -------*/
     
+    /*
     app.get('/new/:id', function(req, res){
         console.log("entro a new");
         TestSchema.findOne({access_code: req.params.id})
@@ -209,9 +253,12 @@ module.exports = (app, passport) => {
             }
         });
     });
+    */
 
     /* --------- REGISTRAR NUEVO ACCESS CODE (PRUEBA NO SE USA) ------- */
 
+    
+    /*
     app.post('/new', function(req, res){
         console.log('Ingreso');
         var newTestSchema = new TestSchema();
@@ -225,27 +272,48 @@ module.exports = (app, passport) => {
             }
         });
      });
+     */
 
+    /*------------- UPDATE id_inst FIND access_code ------------*/
 
-    /*---------- UPDATE id_inst FIND access_code ----------*/
-    
-    app.put('/new/:id', function(req, res){
+    /*----------- VERIFICA SI EXISTE EL ACCESS CODE  -----------*/
+
+    app.post('/new', function(req, res){
         console.log('Update participante');
-        TestSchema.findOneAndUpdate({access_code: req.params.id},
-        {
-            $set: {id_inst: req.body.id_inst}
-        },
-        {
-            new: true
-        },
-        function(err, updateTest){
+
+        var newTestSchema = new TestSchema();
+        var access_code = req.body.access_code;
+        
+        TestSchema.findOne({access_code: access_code}, function(err, test){
             if(err){
-                res.send("Error actualizando");
-            }else{
-                res.json(updateTest);
+                console.log(err);
+                    console.log(err); 
                 }
-            }
-        )
+                if(!test){
+                    console.log("no existe")
+                }else{
+                    console.log("si existe");
+                    /*----------- SI EXISTE, SE HAYA EL DOC Y SE ACTUALIZA -------*/
+                    newTestSchema.access_code = req.body.access_code;
+                    TestSchema.findOneAndUpdate({access_code: req.body.access_code},
+                    {
+                        $set: {id_inst: req.body.id_inst}
+                    },
+                    {
+                        new: true
+                    },
+                    function(err, updateTest){
+                        if(err){
+                            res.send("Error actualizando");
+                        }else{
+                            res.json(updateTest);
+                            }
+                        }
+                    )
+                }
+        });
+        
+        
     });
     
     /*
