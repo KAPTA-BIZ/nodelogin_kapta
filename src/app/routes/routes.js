1550946ed9dc9063aa970c6571f9bcfc52dadc2e
Leonardo Garzón <legarzonro@unal.edu.co>
2018-08-23 10:20:58 -0500

    update info from classmarker
    the function was added only for superadmin users. it can be used when a test is assigned to an user.

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -474,8 +474,6 @@ module.exports = (app, passport) => {
                 results.forEach(function (item) {
                     exceptions.push(item.test_id);
                 });
-                console.log("las excepciones son");
-                console.log(exceptions);
             }
             API_Test.find({ 'test_id': { $nin: exceptions } }, null, { sort: { test_name: 1 } }, (err, tests) => {
                 if (err) { throw err }
@@ -511,6 +509,103 @@ module.exports = (app, passport) => {
                 })
             })
         });
+    })
+
+    app.get('/setnewtest_updated/:id', isLoggedIn, (req, res) => {
+        var testsArray = new Array;
+        request(get_all_available, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                var cleanBody = JSON.parse(body);
+                var LinksNumber = cleanBody['links'].length;
+                if (LinksNumber > 0) {
+                    var newTest = new Object;
+                    newTest.test_id = cleanBody['links'][0]['link']['assigned_tests'][0]['test']['test_id'];
+                    newTest.test_name = cleanBody['links'][0]['link']['assigned_tests'][0]['test']['test_name'];
+        
+                    var newLink = new Object;
+                    newLink.link_name = cleanBody['links'][0]['link']['link_name'];
+                    newLink.link_url_id = cleanBody['links'][0]['link']['link_url_id'];
+                    newLink.link_id = cleanBody['links'][0]['link']['link_id'];
+                    newLink.access_list_id = cleanBody['links'][0]['link']['access_list_id'];
+                    newTest.links = [newLink];
+                    testsArray.push(newTest);
+        
+                    for (var j = 1; j < LinksNumber; j++) {
+                        var testFromJson = cleanBody['links'][j]['link']['assigned_tests'][0]['test']['test_id'];
+                        for (var i = 0; i < testsArray.length; i++) {
+                            if (testsArray[i].test_id == testFromJson) {
+                                var newLink = new Object;
+                                newLink.link_name = cleanBody['links'][j]['link']['link_name'];
+                                newLink.link_url_id = cleanBody['links'][j]['link']['link_url_id'];
+                                newLink.link_id = cleanBody['links'][j]['link']['link_id'];
+                                newLink.access_list_id = cleanBody['links'][j]['link']['access_list_id'];
+                                testsArray[i].links.push(newLink);
+                                i = testsArray.lengt;
+                            } else if (i == testsArray.length - 1) {
+                                var newTest = new Object;
+                                newTest.test_id = testFromJson;
+                                newTest.test_name = cleanBody['links'][j]['link']['assigned_tests'][0]['test']['test_name'];
+                                var newLink = new Object;
+                                newLink.link_name = cleanBody['links'][j]['link']['link_name'];
+                                newLink.link_url_id = cleanBody['links'][j]['link']['link_url_id'];
+                                newLink.link_id = cleanBody['links'][j]['link']['link_id'];
+                                newLink.access_list_id = cleanBody['links'][j]['link']['access_list_id'];
+                                newTest.links = [newLink];
+                                testsArray.push(newTest);
+                                i = testsArray.lengt;
+                            }
+                        }
+                    }
+        
+                }
+        
+                API_Test.remove({}, (err) => {
+                    if (!err) { }
+                    updateTests(0, testsArray.length);
+                })
+            } else {
+                //->se debe manejar este error (no se pudo actualizar)
+                console.log(error);
+            }
+        
+        });
+        
+        function updateTests(i, imax) {
+            if (i < imax) {
+        
+                var newAPI_Test = new API_Test({
+                    test_id: testsArray[i].test_id,
+                    test_name: testsArray[i].test_name,
+                    links: testsArray[i].links
+                });
+        
+                newAPI_Test.save((err) => {
+                    if (err) console.log(err);
+                    //--->se debe manejar este error al guardar datos
+                    updateTests(i + 1, imax)
+                })
+        
+            } else {
+                Assignments.find({ 'user_id': req.params.id }, 'test_id', (err, results) => {
+                    if (err) { throw err }
+                    var exceptions = [];
+                    if (results.length > 0) {
+                        results.forEach(function (item) {
+                            exceptions.push(item.test_id);
+                        });
+                    }
+                    API_Test.find({ 'test_id': { $nin: exceptions } }, null, { sort: { test_name: 1 } }, (err, tests) => {
+                        if (err) { throw err }
+                        console.log("estoy aca");
+                        console.log(tests.length);
+                        res.render('set_new_test', {
+                            user: req.params.id,
+                            Tests: tests
+                        });
+                    })
+                })
+            }
+        }
     })
 
     /*-------------- REGISTRAR LINK --------------*/
